aac80711f8e9a3fb46a28c8fcde0103a557d9475
jiangjiawei1103 <waynechuang97@gmail.com>
2023-09-24 05:20:26 +0800

    refactor: Rename evaluation data loader

--- data/build.py
+++ data/build.py
@@ -4,6 +4,8 @@ Author: JiaWei Jiang
 
 This file contains the basic logic of building dataloaders for training
 and evaluation processes.
+
+* [ ] Modify dataloader building logic (maybe one at a time).
 """
 from typing import Any, Optional, Tuple, Type, Union
 
@@ -18,7 +20,7 @@ from .dataset import BenchmarkDataset, TrafficDataset
 def build_dataloaders(
     dataset_name: str,
     data_tr: np.ndarray,
-    data_val: np.ndrray,
+    data_eval: np.ndarray,
     batch_size: int,
     shuffle: bool,
     num_workers: int,
@@ -29,7 +31,7 @@ def build_dataloaders(
     Parameters:
         dataset_name: name of the dataset
         data_tr: training data
-        data_val: validation data
+        data_eval: evaluation data, either validation or test
         batch_size: number of samples per batch
         shuffle: whether to shuffle samples every epoch
         num_workers: number of subprocesses used to load data
@@ -37,7 +39,7 @@ def build_dataloaders(
 
     Return:
         train_loader: training data loader
-        val_loader: validation data loader
+        eval_loader: evaluation data loader
     """
     dataset: Union[Type[BenchmarkDataset], Type[TrafficDataset]] = None
     if dataset_name in MTSFBerks:
@@ -54,15 +56,15 @@ def build_dataloaders(
         num_workers=num_workers,
         collate_fn=collate_fn,
     )
-    if data_val is not None:
-        val_loader = DataLoader(
-            dataset(data_val, **dataset_cfg),
+    if data_eval is not None:
+        eval_loader = DataLoader(
+            dataset(data_eval, **dataset_cfg),
             batch_size=batch_size * 4,  # 1024 for solar
             shuffle=False,  # Hard-coded
             num_workers=num_workers,
             collate_fn=collate_fn,
         )
 
-        return train_loader, val_loader
+        return train_loader, eval_loader
     else:
         return train_loader, None
